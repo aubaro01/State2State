5c340700fdf1490e966f2bb998a74b0536666394
Cláudio Barroso <109551603+aubaro01@users.noreply.github.com>
2025-05-07 15:55:06 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const app = express();
+const path = require('path');
+
 
 app.use(express.json());
 app.use(helmet());
